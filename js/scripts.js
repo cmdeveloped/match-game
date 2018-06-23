8d73e95b103a31ec7eee80a15af0fb91283daf30
cmdeveloped <cmwebdevs@gmail.com>
2018-06-23 14:18:38 -0500

    main functionality finished and moves counter working

--- js/scripts.js
+++ js/scripts.js
@@ -1,25 +1,56 @@
+/*eslint-disable no-unused-vars*/
 $(document).ready(function() {
   // write your scripts here
 
   let shuffle = () => {
-    // empty cards to reset
-    $('.cards').empty();
     // create icons array and sort
-    let icons = ['fal fa-plane', 'fal fa-car', 'fal fa-bicycle', 'fal fa-fighter-jet', 'fal fa-rocket', 'fal fa-ship', 'fal fa-plane', 'fal fa-car', 'fal fa-bicycle', 'fal fa-fighter-jet', 'fal fa-rocket', 'fal fa-ship'];
-    icons.sort(function() {
+    let icons = [
+      {
+        slug: 'plane',
+        class: 'fal fa-plane'
+      },
+      {
+        slug: 'car',
+        class: 'fal fa-car'
+      },
+      {
+        slug: 'bike',
+        class: 'fal fa-bicycle'
+      },
+      {
+        slug: 'jet',
+        class: 'fal fa-fighter-jet'
+      },
+      {
+        slug: 'rocket',
+        class: 'fal fa-rocket'
+      },
+      {
+        slug: 'ship',
+        class: 'fal fa-ship'
+      },
+    ];
+    // create empty array for duplicating icons
+    let pairs = [];
+    for (let icon of icons) {
+      pairs.push(icon);
+      pairs.push(icon);
+    }
+    // sort pairs into random order
+    pairs.sort(function() {
       return 0.5 - Math.random();
     });
 
     // card template set up
-    function gameCard(icon) {
+    function gameCard(icons) {
       const cardHTML = `
       <div class="game-card">
         <div class="inner">
           <div class="front">
-            <i class="fal fa-question-circle"></i>
+            <i class="fal fa-question"></i>
           </div>
-          <div class="back">
-            <i class="${icon}"></i>
+          <div class="back" data-match="${icons.slug}">
+            <i class="${icons.class}"></i>
           </div>
         </div>
       </div>
@@ -29,32 +60,76 @@ $(document).ready(function() {
     }
 
     // loop through icons and create card for each
-    for ( let icon of icons ) {
-      $('.cards').append(gameCard(icon));
+    for ( let card of pairs ) {
+      $('.cards').append(gameCard(card));
     }
 
     $('.cards').removeClass('load');
     $('.cards').addClass('load');
   }
+
   // initial shuffle function called
   shuffle();
 
+  // keep track of moves
+  let count = 0;
+  let moves = 0;
+  let countMoves = () => {
+    count++;
+    if (count % 2 === 0) {
+      moves++;
+      $('#moves').text(moves);
+    }
+  }
+
+  // define reset function
+  let reset = () => {
+    // empty cards to reset
+    $('.cards').empty();
+    // reset moves count
+    count = 0;
+    moves = 0;
+    $('#moves').text(moves);
+  }
+
   // refresh all cards when button is clicked
   $('#refresh').click(function() {
+    reset();
     shuffle();
   });
 
-  // click card and toggle active
-  let count = 0;
+
+  // create empty array to store clicked active cards
   const clickedCards = [];
+
+  // click card and toggle active
   $(document).on('click', '.game-card', function() {
-    let clickedCard = $(this).find('.back i').attr('class');
-    $(this).addClass('active');
+    // get the name of card
+    let clickedCard = $(this).find('.back').data('match');
+    // push the clicked card's name to the array
     clickedCards.push(clickedCard);
-    count++;
-    if (count === 2) {
-      count = 0;
+    // add move
+    countMoves();
+    // remove any active classes from matched cards
+    $('.game-card.matched').removeClass('active');
+    // add class active to clicked card to reveal image
+    $(this).addClass('active');
+    // check cards to check for matches and reset array
+    if (clickedCards.length === 2) {
+      if (clickedCards[0] === clickedCards[1]) {
+        setTimeout(function() {
+          $('.game-card.active').addClass('matched');
+        }, 2000);
+      } else {
+        setTimeout(function() {
+          $('.game-card.active').removeClass('active');
+        }, 2000);
+      }
+      // set array to empty
+      clickedCards.length = 0;
     }
     console.log(clickedCards);
   });
+
+
 });
